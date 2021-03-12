7f6ecbfaebc646f975c332a50d5ffd637298e6db
Carlos Ospina <carlossospinag@gmail.com>
2021-03-12 15:46:27 -0500

    update events and users tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_12_202958) do
+ActiveRecord::Schema.define(version: 2021_03_12_204407) do
 
   create_table "events", force: :cascade do |t|
     t.string "date"
     t.string "location"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_events_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "username"
   end
 
 end
