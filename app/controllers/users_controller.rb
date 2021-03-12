ece64dead0ccc63c4cdc64d2282d89ff34f2160b
Carlos Ospina <carlossospinag@gmail.com>
2021-03-12 16:48:15 -0500

    add actions to users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,25 @@
 class UsersController < ApplicationController
-    
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            session[:user_id] = @user.id
+            redirect_to @user, notice: "Succesfully created account"
+        else
+            render :new
+        end
+      end
+
+    def show
+        @user = User.find(params[:id])
+    end
+
+    private
+    # Only allow a list of trusted parameters through.
+    def user_params
+      params.require(:user).permit(:username)
+    end
 end
