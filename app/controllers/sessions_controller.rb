9c7c3371bb7aa8c019255455de5d4ed173e43a88
Carlos Ospina <carlossospinag@gmail.com>
2021-03-15 16:16:25 -0500

    update sessions controller and events controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,13 +4,11 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(username: params[:session][:username].downcase)
-    # if user && user.authenticate(params[:session][:password])
-    if user && user.authenticate(params[:session])
+    if user 
       log_in user
       redirect_back_or user
     else
-      # flash.now[:danger] = 'Invalid email/password combination'
-      flash.now[:danger] = 'Invalid username'
+      flash.now[:danger] = "Username doesn't exist"
       render 'new'
     end
   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+    before_action :logged_in_user, only: [:show]
     def new
         @user = User.new
     end
@@ -6,13 +7,14 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
-            session[:user_id] = @user.id
-            redirect_to @user, notice: "Succesfully created account"
+            log_in @user
+            flash[:success] = "Welcome to the app!"
+            redirect_to @user
         else
             render :new
         end
       end
-
+                 
     def show
          @user = User.find(params[:id])
     end
