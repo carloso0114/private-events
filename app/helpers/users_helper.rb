b66a237caae88e6428e5d02b60117f79f78aaa5e
unknown <paulclue20@gmail.com>
2021-03-15 18:56:07 -0500

    update show page for users

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,14 @@
 module UsersHelper
+  def u_list(use, eve)
+    table = ''
+     eve.each do |event| 
+      if use.id == event.user_id
+       table += render inline: '<tr>'
+       table += render inline: "<td><%= #{event.user_id} %></td>"
+       table += render inline: "<td> #{event.location} </td>"
+       table += render inline: '</tr>'
+      end
+    end
+      table.html_safe
+  end
 end
