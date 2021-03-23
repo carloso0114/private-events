710bc2b647f3bb66af2c3f2a61d3dbb92a10a73f
Carlos Ospina <carlossospinag@gmail.com>
2021-03-23 17:45:27 -0500

    update users show view to display attenting events

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -4,10 +4,19 @@ module UsersHelper
      eve.each do |event|
       if use.id == event.creator_id.to_i
        table += render inline: '<tr>'
-       table += render inline: "<td> #{event.description} </td>"
+       table += render inline: "<td>#{event.description} </td>"
        table += render inline: '</tr>'
       end
     end
       table.html_safe
   end
+
+  def attending
+    name = ''
+        @user.attended_events.each do |att|
+            name += render inline: "<p>#{att.description}</p>"
+        end
+    name.html_safe
+  end
+
 end
