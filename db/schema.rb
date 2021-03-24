196222f940d626105cc466dff0074110ff5666bb
Carlos Ospina <carlossospinag@gmail.com>
2021-03-24 10:20:16 -0500

    update schema datatype for date

--- db/schema.rb
+++ db/schema.rb
@@ -22,7 +22,7 @@ ActiveRecord::Schema.define(version: 2021_03_19_234934) do
   end
 
   create_table "events", force: :cascade do |t|
-    t.string "date"
+    t.date "date"
     t.string "location"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
