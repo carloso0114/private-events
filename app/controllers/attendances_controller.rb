0dffefe4cda881d66bccbce614f4bf3b73f0ce10
Carlos Ospina <carlossospinag@gmail.com>
2021-03-24 16:31:02 -0500

    fix rubocop errors, update readme file

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,18 +1,13 @@
 class AttendancesController < ApplicationController
+  def create
+    @attendance = Attendance.create(event_id: params[:event_id], user_id: current_user.id)
+    redirect_to event_path if @attendance.save
+  end
 
-    def create
-        @attendance = Attendance.create(event_id: params[:event_id], user_id: current_user.id)
-        if @attendance.save 
-            redirect_to event_path
-        end
-        
-    end
-    private
-    # Only allow a list of trusted parameters through.
-    def attendance_params
-      params.require(:attendances).permit(:user_id, :event_id)
-    end
+  private
+
+  # Only allow a list of trusted parameters through.
+  def attendance_params
+    params.require(:attendances).permit(:user_id, :event_id)
+  end
 end
-
-
-
