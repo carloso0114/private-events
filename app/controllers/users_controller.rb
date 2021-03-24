0bc0228a88cfdfa133d26003a84d18cd92413faf
Carlos Ospina <carlossospinag@gmail.com>
2021-03-24 10:42:08 -0500

    modify User's show page to show upcoming/previous events

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
     before_action :logged_in_user, only: [:show]
+    
     def new
         @user = User.new
     end
@@ -18,6 +19,8 @@ class UsersController < ApplicationController
     def show
       @user = User.find(params[:id])
       @events = Event.all
+ #     @date = User.attended_events
+      @time = Time.now
     end
 
     private
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -13,10 +13,26 @@ module UsersHelper
 
   def attending
     name = ''
+   
         @user.attended_events.each do |att|
+          if att.date > @time
             name += render inline: "<p>#{att.description}</p>"
-        end
+            name += render inline: "<p>#{att.date}</p>"
+          end
+      end
     name.html_safe
   end
+
+  def attending_past
+    name = ''
+        @user.attended_events.each do |att|
+          if att.date < @time
+            name += render inline: "<p>#{att.description}</p>"
+            name += render inline: "<p>#{att.date}</p>"
+          end
+      end
+    name.html_safe
+  end
+
 
 end
