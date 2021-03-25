50f9d3511bdf405f23e9bb78d24779e505607c1e
Carlos Ospina <carlossospinag@gmail.com>
2021-03-25 17:47:42 -0500

    add associations tests using rspec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,30 +1,4 @@
 require "rails_helper"
-require "spec_helper"
-
-
-
-# Disable Transactions
-RSpec.configure do |c|
-  c.use_transactional_examples = false
-  c.use_transactional_fixtures = false
-  c.order = "defined"
-end
-
-# RSpec.describe User, :type => :model do
-#   it "has none to begin with" do
-#     expect(User.count).to eq 0
-#   end
-
-#   it "has one after adding one" do
-#     User.create
-#     expect(User.count).to eq 1
-#   end
-
-#   it "has none after one was created in a previous example" do
-#     expect(User.count).to eq 0
-#   end
-# end
-
 
 RSpec.describe User, :type => :model do
   it "has none to begin with" do
@@ -36,9 +10,42 @@ RSpec.describe User, :type => :model do
     expect(User.count).to eq 1
   end
 
-  it "has one after one was created in a previous example" do
-    expect(User.count).to eq 1
+  it "has none after one was created in a previous example" do
+    expect(User.count).to eq 0
   end
-
-  after(:all) { User.destroy_all }
 end
+
+RSpec.describe Attendance, type: :model do
+  it { should belong_to(:user) }
+  it { should belong_to(:event) }
+end
+
+RSpec.describe Event, type: :model do
+  it { should have_many(:attendances) }
+  it { should have_many(:attendees) }
+  it { should belong_to(:creator) }
+end
+
+RSpec.describe User, type: :model do
+  it { should have_many(:created_events) }
+  it { should have_many(:attendances) }
+  it { should have_many(:attended_events) }
+end
+
+# RSpec.describe 'Open Browser test', type: :feature do
+#   scenario 'Sign up page' do
+#     visit new_user_path
+#     sleep(10)
+#     expect(page).to have_content('Navbar\nEvents       New Event       Back      \nLog in       Sign up\nSign up\nUsername')
+#   end
+# end
+
+# RSpec.describe 'Open Browser test', type: :feature do
+#   scenario 'Sign up page' do   
+#       visit new_user_path
+#       fill_in "Username", :with => 'carlos'
+#       click_button "Sign Up"
+#       gets 'users_path'
+#       response.should be_success
+#   end
+# end
