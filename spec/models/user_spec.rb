bdfce9ddf835421bba08ac95fe8cfc4fb05424c3
unknown <paulclue20@gmail.com>
2021-03-26 14:36:15 -0500

    correct linters and delete unnecessary event function

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,32 +1,35 @@
-require "rails_helper"
+require 'rails_helper'
 
-RSpec.describe User, :type => :model do
-  it "has none to begin with" do
+RSpec.describe User, type: :model do
+  it 'has none to begin with' do
     expect(User.count).to eq 0
   end
 
-  it "has one after adding one" do
+  it 'has one after adding one' do
     User.create
     expect(User.count).to eq 1
   end
 
-  it "has none after one was created in a previous example" do
+  it 'has none after one was created in a previous example' do
     expect(User.count).to eq 0
   end
 end
 
-RSpec.describe Event, :type => :model do
-  it "has none to begin with" do
-    expect(Event.count).to eq 0
+RSpec.describe 'Create new event', type: :feature do
+  before :each do
+    User.create(username: 'carlos')
   end
 
-  it "has one after adding one" do
-    Event.create
-    expect(User.count).to eq 1
-  end
-
-  it "has none after one was created in a previous example" do
-    expect(Event.count).to eq 0
+  it 'signs in and create event' do
+    visit login_path
+    within('.login-form') do
+      fill_in 'Username', with: 'carlos'
+    end
+    click_button 'Log in'
+    visit new_event_path
+    fill_in 'Description', with: 'Party'
+    click_button 'commit'
+    expect(current_path).to eq('/events/1')
   end
 end
 
@@ -35,19 +38,19 @@ RSpec.describe 'Create new attendance', type: :feature do
     User.create(username: 'carlos')
   end
 
-    it 'creates a new attendance' do
-      visit login_path
-      within('.login-form') do
-      fill_in "Username", :with => 'carlos'
+  it 'creates a new attendance' do
+    visit login_path
+    within('.login-form') do
+      fill_in 'Username', with: 'carlos'
     end
-      click_button "Log in"
-      visit new_event_path
-      fill_in "Description", :with => 'Party'
-      click_button "commit"
-      click_button "commit"
-      expect(Attendance.first.user_id).to eq(User.first.id)
-      expect(Attendance.first.event_id).to eq(Event.first.id)
-    end
+    click_button 'Log in'
+    visit new_event_path
+    fill_in 'Description', with: 'Party'
+    click_button 'commit'
+    click_button 'commit'
+    expect(Attendance.first.user_id).to eq(User.first.id)
+    expect(Attendance.first.event_id).to eq(Event.first.id)
+  end
 end
 
 RSpec.describe Attendance, type: :model do
@@ -72,46 +75,23 @@ RSpec.describe 'Sign in test', type: :feature do
     User.create(username: 'carlos')
   end
 
-    it 'signs me in' do
-      visit login_path
-      within('.login-form') do
-      fill_in "Username", :with => 'carlos'
+  it 'signs me in' do
+    visit login_path
+    within('.login-form') do
+      fill_in 'Username', with: 'carlos'
     end
-      click_button "Log in"
-      expect(current_path).to eq("/users/1")
-    end 
+    click_button 'Log in'
+    expect(current_path).to eq('/users/1')
+  end
 end
 
 RSpec.describe 'Sign up test', type: :feature do
-
-    it 'create new account' do
-      visit new_user_path
-      within('.sign-up-form') do
-      fill_in "Username", :with => 'carlos'
+  it 'create new account' do
+    visit new_user_path
+    within('.sign-up-form') do
+      fill_in 'Username', with: 'carlos'
     end
-      click_button "Sign Up"
-      expect(current_path).to eq("/users/1")
-    end 
-end
-
-RSpec.describe 'Create new event', type: :feature do
-  before :each do
-    User.create(username: 'carlos')
+    click_button 'Sign Up'
+    expect(current_path).to eq('/users/1')
   end
-
-    it 'signs in and create event' do
-      visit login_path
-      within('.login-form') do
-      fill_in "Username", :with => 'carlos'
-    end
-      click_button "Log in"
-      visit new_event_path
-      fill_in "Description", :with => 'Party'
-      click_button "commit"
-      expect(current_path).to eq("/events/1")
-    end
 end
-
-
-
-
